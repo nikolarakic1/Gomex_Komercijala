56751e6d063337aaea24f2f29c221a8b40557af0
PetarKercov <kercovpetar@gmail.com>
2026-08-11 10:19:50 +0200

    Obrisan stari fajl

--- GomexMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GomexMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
