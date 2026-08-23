04e3bc83fa5f73fc4b3b33ac12c5c65ce11063ec
PetarKercov <kercovpetar@gmail.com>
2026-08-24 00:27:49 +0200

    Login screen

---
+++ GomexPraksaMVC/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GomexPraksaMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Korisničko ime je obavezno.")]
+        [Display(Name = "Korisničko ime")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Lozinka je obavezna.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Lozinka")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Zapamti me")]
+        public bool RememberMe { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
