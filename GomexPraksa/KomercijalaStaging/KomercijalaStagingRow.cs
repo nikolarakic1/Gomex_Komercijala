27be078a9f64d1df172dc9c1fb9309fe559f22d2
Nikola Rakic <nikolarakic2003@gmail.com>
2026-09-01 09:50:27 +0200

    fas

---
+++ GomexPraksa/KomercijalaStaging/KomercijalaStagingRow.cs
@@ -0,0 +1,35 @@
+﻿namespace GomexPraksa.KomercijalaStaging
+{
+    public class KomercijalaStagingRow
+    {
+        public string? Tip { get; set; }
+        public int? Godina { get; set; }
+        public int? Mesec { get; set; }
+        public int? Nedelja { get; set; }
+
+        public string? IdKampanja1 { get; set; }
+        public string? IdKampanja2 { get; set; }
+        public string? CmUtice { get; set; }
+
+        public string? Odeljenje { get; set; }
+        public string? Kategorija { get; set; }
+        public string? RobnaGrupa { get; set; }
+
+        public string? Sifra { get; set; }
+        public string? Artikal { get; set; }
+
+        public decimal? Pdv { get; set; }
+
+        public string? Dobavljac { get; set; }
+
+        public decimal? Kolicina { get; set; }
+        public decimal? MpVrednost { get; set; }
+        public decimal? MpBezPdv { get; set; }
+
+        public decimal? Ruc1 { get; set; }
+        public decimal? Ruc2 { get; set; }
+        public decimal? Ruc12 { get; set; }
+
+        public decimal? NabavnaVrednost { get; set; }
+    }
+}
---
+++ GomexPraksa/KomercijalaStaging/StagingRepo.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace GomexPraksa.KomercijalaStaging
+{
+    public class StagingRepo : IStagingRepo
+    {
+        private readonly string _connectionString;
+        public StagingRepo(IConfiguration configuration)
+        {
+            _connectionString =
+                configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException(
+                    "Connection string nije pronađen.");
+        }
+        public async Task BulkInsertStagingAsync(DataTable table)
+        {
+            await using var connections = new SqlConnection(_connectionString);
+            await connections.OpenAsync();
+            using var bulkCopy =new SqlBulkCopy(connections);
+            bulkCopy.DestinationTableName =
+            "dbo.KomercijalaImportStaging";
+            bulkCopy.BatchSize = 10000;
+            bulkCopy.BulkCopyTimeout = 120;
+            bulkCopy.ColumnMappings.Add("Tip", "Tip");
+            bulkCopy.ColumnMappings.Add("Godina", "Godina");
+            bulkCopy.ColumnMappings.Add("Mesec", "Mesec");
+            bulkCopy.ColumnMappings.Add("Nedelja", "Nedelja");
+
+            bulkCopy.ColumnMappings.Add(
+                "Odeljenje",
+                "Odeljenje");
+
+            bulkCopy.ColumnMappings.Add(
+                "Kategorija",
+                "Kategorija");
+
+            bulkCopy.ColumnMappings.Add(
+                "RobnaGrupa",
+                "RobnaGrupa");
+
+            bulkCopy.ColumnMappings.Add(
+                "Sifra",
+                "Sifra");
+
+            bulkCopy.ColumnMappings.Add(
+                "Artikal",
+                "Artikal");
+
+            bulkCopy.ColumnMappings.Add(
+                "Dobavljac",
+                "Dobavljac");
+
+            bulkCopy.ColumnMappings.Add(
+                "Kolicina",
+                "Kolicina");
+
+            bulkCopy.ColumnMappings.Add(
+                "MPBezPDV",
+                "MPBezPDV");
+
+            bulkCopy.ColumnMappings.Add(
+                "RUC1",
+                "RUC1");
+
+            bulkCopy.ColumnMappings.Add(
+                "RUC2",
+                "RUC2");
+
+            bulkCopy.ColumnMappings.Add(
+                "RUC12",
+                "RUC12");
+
+            bulkCopy.ColumnMappings.Add(
+                "NabavnaVrednost",
+                "NabavnaVrednost");
+            await bulkCopy.WriteToServerAsync(table);
+        }
+    }
+}
